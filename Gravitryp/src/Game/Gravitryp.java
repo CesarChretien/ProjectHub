deedeee01a41f4c08eefced09b00e4d188d45022
César Chrétien <cesarchre10@gmail.com>
2016-04-12 16:32:44 +0200

    game getting there.

---
+++ Gravitryp/src/Game/Gravitryp.java
@@ -0,0 +1,10 @@
+package Game;
+
+import Map.Space;
+
+public class Gravitryp {
+
+	public static void main(String[] args) {
+		new Space();
+	}
+}
---
+++ Gravitryp/src/Map/Space.java
@@ -0,0 +1,14 @@
+package Map;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Space extends JFrame {
+
+	public Space() {
+		this.setLocation(100, 100);
+		this.setSize(800, 600);
+		this.setVisible(true);
+	}
+}
