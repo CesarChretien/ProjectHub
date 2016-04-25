b9d3be2940c02a9bd9a61cd140c87ea833ee56ea
César Chrétien <cesarchre10@gmail.com>
2016-04-25 16:39:46 +0200

    mooie dingen.

--- OnlineGravitryp/WebContent/resources/Planet.js
+++ OnlineGravitryp/WebContent/resources/Planet.js
@@ -6,4 +6,9 @@ function Planet(centre, radius, gc) {
 	this.sprite = new Sprite(centre, "circle", 2*radius, 2*radius, new Point(1, 0));
 	this.radius = radius;
 	this.gc = gc;
+	
+	this.relocate = function(newx, newy) {
+		this.sprite.centre = new Point(newx, newy);
+		this.sprite.setHitbox(this.sprite.centre, new Point(1, 0));
+	}
 }
